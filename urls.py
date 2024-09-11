987f29bb80ff8a0976e97dba430288bb4eddab9e
Nace Zavrtanik <zavrtaniknace@gmail.com>
2024-09-12 01:46:45 +0200

    main.py split into modules urls.py, prices.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+
+CARDS = {
+    "rabbit": "https://www.cardmarket.com/en/YuGiOh/Products/Singles/The-Infinite-Forbidden/Silhouhatte-Rabbit-V1-Secret-Rare",
+    "vampire_lord": "https://www.cardmarket.com/en/YuGiOh/Products/Singles/Dark-Crisis/Vampire-Lord-V2-Secret-Rare",
+    "breaker": "https://www.cardmarket.com/en/YuGiOh/Products/Singles/Magicians-Force/Breaker-the-Magical-Warrior",
+}
+
+def generate_links():
+    return CARDS.items()
+
