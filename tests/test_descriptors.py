2c6a7ca100c8f4d6f38bf83ebacfc2eb233de49e
Nace Zavrtanik <zavrtaniknace@gmail.com>
2024-10-19 22:25:46 +0200

    Tests for OneOf slightly improved

--- tests/test_descriptors.py
+++ tests/test_descriptors.py
@@ -7,6 +7,7 @@ from cardmarketwatch.descriptors import UpperString, OneOf
 def test_upper_string_fails_when_assigned_non_string():
     class C:
         string = UpperString()
+
     instance = C()
     with pytest.raises(TypeError):
         instance.string = 123
@@ -15,6 +16,7 @@ def test_upper_string_fails_when_assigned_non_string():
 def test_upper_string_gives_uppercase_string():
     class C:
         string = UpperString()
+
     instance = C()
     instance.string = "it's time to duel!"
     expected = "IT'S TIME TO DUEL!"
@@ -24,6 +26,7 @@ def test_upper_string_gives_uppercase_string():
 def test_upper_string_works_for_multiple_attribute_assignments():
     class C:
         string = UpperString()
+
     instance = C()
     instance.string = "monsuta caado"
     instance.string = "card games on motorcycles"
@@ -32,12 +35,11 @@ def test_upper_string_works_for_multiple_attribute_assignments():
     assert instance.string == expected
 
 
-def test_one_of():
+def test_one_of_works_for_multiple_attribute_assignments():
     class Letter(enum.StrEnum):
         A = "a"
         B = "b"
         C = "c"
-
     class C:
         letter = OneOf(Letter, default=Letter.A)
 
@@ -46,6 +48,17 @@ def test_one_of():
     instance.letter = None
     assert instance.letter is None
     instance.letter = "b"
-    instance.letter is Letter.B
+    assert instance.letter is Letter.B
+
+
+def test_one_of_fails_when_assigned_invalid_string():
+    class Letter(enum.StrEnum):
+        A = "a"
+        B = "b"
+        C = "c"
+    class C:
+        letter = OneOf(Letter, default=Letter.A)
+
+    instance = C()
     with pytest.raises(ValueError):
         instance.letter = "d"
